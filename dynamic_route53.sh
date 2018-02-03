649f9274d53b65f3a9059c7c94fe022352108728
Mat Pataki <matpataki@gmail.com>
2018-02-03 00:35:14 -0500

    working CF template + script

--- dynamic_route53.sh
+++ dynamic_route53.sh
@@ -1,2 +1,12 @@
 
 PUBLIC_IP=`dig TXT +short o-o.myaddr.l.google.com @ns1.google.com`
+HOSTED_ZONE=$1
+
+aws cloudformation create-stack \
+  --profile personal \
+  --stack-name HomeDynamicDNS \
+  --template-body file://home_record_set.yml \
+  --parameters \
+      ParameterKey=IP,ParameterValue=$PUBLIC_IP \
+      ParameterKey=HostedZoneName,ParameterValue=$HOSTED_ZONE
+
