fd2e8ad8a02dc912c66ce8faf70c9f009c1d29b0
Mat Pataki <matpataki@gmail.com>
2018-02-03 00:56:56 -0500

    add IAM user

--- dynamic_route53.sh
+++ dynamic_route53.sh
@@ -1,12 +1,15 @@
 
 PUBLIC_IP=`dig TXT +short o-o.myaddr.l.google.com @ns1.google.com`
-HOSTED_ZONE=$1
+HOSTED_ZONE_NAME=$1
+HOSTED_ZONE_ID=$2
 
-aws cloudformation create-stack \
+aws cloudformation update-stack \
   --profile personal \
   --stack-name HomeDynamicDNS \
   --template-body file://home_record_set.yml \
+  --capabilities CAPABILITY_NAMED_IAM \
   --parameters \
       ParameterKey=IP,ParameterValue=$PUBLIC_IP \
-      ParameterKey=HostedZoneName,ParameterValue=$HOSTED_ZONE
+      ParameterKey=HostedZoneName,ParameterValue=$HOSTED_ZONE_NAME \
+      ParameterKey=HostedZoneID,ParameterValue=$HOSTED_ZONE_ID \
 
